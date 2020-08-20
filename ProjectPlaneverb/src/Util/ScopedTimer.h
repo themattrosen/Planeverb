44f6d348fb722f85fa79dc6df2ae8fe927e73503
Nikunj Raghuvanshi <nikunjr@microsoft.com>
2020-08-20 16:05:08 -0700

    Updated README.

--- ProjectPlaneverb/src/Util/ScopedTimer.h
+++ ProjectPlaneverb/src/Util/ScopedTimer.h
@@ -30,7 +30,7 @@ namespace Planeverb
 			auto end = std::chrono::time_point_cast<std::chrono::microseconds>(endTimePoint).time_since_epoch().count();
 
 			auto duration = end - start;
-			m_outputStream << duration << "us";
+			m_outputStream << (duration)/1000.f << "ms";
 		}
 
 	private:
